dd03aa99835f2f1a3f2d5ec5c2785e9b6ee6c2e3
kanika-sharma-pro <kanikasharma1908@gmail.com>
2025-04-06 15:12:43 -0400

    done until button presses to go down

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -2,13 +2,31 @@ package io.zipcoder;
 
 public class Music {
 
-    private String[] playList;
+    private String[] playList; //instance variable to store playlist
 
-    public Music(String[] playList){
+    //constructor to initialize music object with playlist
+    public Music(String[] playList) {
+
         this.playList = playList;
     }
+//method to calculate minimum button presses to select a song
+    public Integer selection(Integer startIndex, String selection) {
+        int n = playList.length;
+        int qIndex = -1;//it tells us that target song is not found in the playlist
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+        //loop through playlist to find the index of the target song
+        for (int i = 0; i < n; i++) {
+            if (playList[i].equals(selection)) {
+                qIndex = i;
+                break;
+            }
+        }
+        if (qIndex == -1) {
+            return -1;
+        }
+        //number of button presses to go down
+        int forwardPresses = 0;
+        int currentIndex = startIndex;//start from the starting index
+
     }
 }
